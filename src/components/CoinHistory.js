cc89ca189aa020019c1b47d2382212a0e84ae808
AMAN VERMA <amankumarverma251@gmail.com>
2023-08-01 17:30:48 +0530

    Final Changes

--- src/components/CoinHistory.js
+++ src/components/CoinHistory.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
@@ -10,17 +10,17 @@ const CoinHistory = () => {
   const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
 
-  const fetchCoin = async () => {
+  const fetchCoin = useCallback(async () => {
     try {
       const { data } = await axios.get(SingleCoin(id));
       setCoinData(data);
       setIsLoading(false);
       console.log(data);
     } catch (error) {
-      console.log(error);
+      console.error('Error fetching coin data:', error);
       setIsLoading(false);
     }
-  };
+  }, [id]);
 
   const fetchPriceData = async (coinId, currency) => {
     try {
@@ -38,17 +38,17 @@ const CoinHistory = () => {
     }
   };
 
-  
-    const fetchData = async () => {
+  useEffect(() => {
+    const fetchCoinData = async () => {
       try {
         await fetchCoin();
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching coin data:', error);
       }
     };
 
-    fetchData();
- 
+    fetchCoinData();
+  }, [fetchCoin]);
 
   useEffect(() => {
     const fetchPrice = async () => {
@@ -70,20 +70,21 @@ const CoinHistory = () => {
       <Wrapper>
         {isLoading ? (
           <div className="loader-for-whole-page">
-            <img src={Loading} alt="loader" />
+            <img src={Loading} alt="Loading..." />
           </div>
         ) : (
-          <div>
-            <div className="side-bar"></div>
-            <img src={coinData.image.large} alt="Coin Icon" className="coin-icon" />
-            <h1>{coinData.name}</h1>
-            <p>{coinData.description.en.split('. ')[0]}</p>
-            <h3>Price: {coinData.price}</h3>
-            <h3>Symbol: {coinData.symbol}</h3>
-            <h2>Rank: {coinData.market_cap_rank}</h2>
-            <h2>Public Interest Score: {coinData.public_interest_score}</h2>
-           
-          </div>
+          coinData && (
+            <div>
+              <div className="side-bar"></div>
+              <img src={coinData.image.large} alt="Coin Icon" className="coin-icon" />
+              <h1>{coinData.name}</h1>
+              <p>{coinData.description.en.split('. ')[0]}</p>
+              <h3>Price: {coinData.price}</h3>
+              <h3>Symbol: {coinData.symbol}</h3>
+              <h2>Rank: {coinData.market_cap_rank}</h2>
+              <h2>Public Interest Score: {coinData.public_interest_score}</h2>
+            </div>
+          )
         )}
       </Wrapper>
     </>
@@ -94,13 +95,12 @@ export default CoinHistory;
 
 const Wrapper = styled.div`
   display: flex;
-  margin: 0px;
-  padding: 0px;
+  margin: 0;
+  padding: 0;
   justify-content: center;
   align-items: center;
-  position: relative;
-  left: -380px;
-  top: -30px;
+  margin-left: -800px;
+  margin-top: -20px;
 
   img {
     height: 30vh;
@@ -110,20 +110,18 @@ const Wrapper = styled.div`
     font-size: 5em;
     color: white;
     font-family: 'Pangolin', cursive;
-    position: relative;
-    top: 20px;
+    margin-top: 20px;
   }
 
   p {
     color: white;
-    font-size: 1.3em;
+    font-size: 1.2em;
     width: 500px;
     overflow-wrap: break-word;
     word-wrap: break-word;
     word-break: break-word;
-    position: relative;
-    left: -100px;
-    top: 20px;
+    margin-left: -100px;
+    margin-top: 20px;
   }
 
   .side-bar {
@@ -133,16 +131,14 @@ const Wrapper = styled.div`
   h3 {
     color: white;
     font-size: 2rem;
-    position: relative;
-    left: -100px;
-    top: 20px;
+    margin-left: -100px;
+    margin-top: 20px;
   }
 
   h2 {
     color: white;
     font-size: 2rem;
-    position: relative;
-    left: -100px;
-    top: 20px;
+    margin-left: -100px;
+    margin-top: 20px;
   }
 `;
